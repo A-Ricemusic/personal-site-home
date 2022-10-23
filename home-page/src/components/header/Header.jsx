b81f38e605c5556b6e072caf1d50b93b515f52cf
A-Ricemusic <anthonyricemath@gmail.com>
2022-10-23 15:24:01 -0400

    Finsishing Header Componenets

--- home-page/src/components/header/Header.jsx
+++ home-page/src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import './Header.css'
+import CTA from './CTA'
+import ImageMe from '../../Assets/ImageMe.Jpeg'
+import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className = "container header__container">
+      <h5>Hello, I am</h5>
+      <h1>Anthony</h1>
+      <h5 className='text-light'> Software Engineer </h5>
+      <CTA />
+      <HeaderSocials/>
+      <div className = "me">
+        <img src={ImageMe} alt = "me"/>
+      </div>
+      <a href='#contact' className='scroll__Down'>Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
---
+++ home-page/src/components/header/HeaderSocials.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BsLinkedin} from 'react-icons/bs'
+
+const HeaderSocials = () => {
+  return (
+    <div className='header__socials'> Socials
+    <a href='https://www.linkedin.com' target= '__blank' >  Linkedin {BsLinkedin}</a>
+    <a href='https://www.github.com' target= '__blank' > Github</a>
+    </div>
+  )
+}
+
+export default HeaderSocials
